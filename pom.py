e2bd44946114ebf7bb31d5f0c8d7e68c992171f8
Serrayon <serrayon81@gmail.com>
2022-10-26 12:44:35 -0700

    working now

--- pom.py
+++ pom.py
@@ -33,8 +33,13 @@ test_value = 'it worked'
 # Test
 trng_page = TrainingGroundPage(driver=browser)
 trng_page.go()
+#assert trng_page.button1.text == 'butn1'
+
+
+
 trng_page.type_into_input(test_value)
-#trng_page.click_button_1()
+trng_page.click_button_1()
 txt_from_input = trng_page.get_input_text()
 assert txt_from_input == test_value, f"Test Failed: Input did not match expected {test_value}."
 print("Test Passed.")
+browser.quit()
